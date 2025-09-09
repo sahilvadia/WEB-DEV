ecb0d037711b8aa30a8f3807478b926ffb3d688b
sahil vadia <sahilvadia7776@gmail.com>
2025-09-10 02:13:23 +0530

    debounce

--- jsProject/throuttlingDebounce/script.js
+++ jsProject/throuttlingDebounce/script.js
@@ -3,6 +3,7 @@ const messageForm = document.getElementById("messageForm");
 const messageContainer = document.querySelector(".messages");
 
 const messageInterval = document.querySelector(".timeinterval");
+const slowSendTime = document.querySelector(".slowsend-time");
 
 
 
@@ -11,10 +12,12 @@ function throttle(func, delay){
     return function(...args){
         const now = new Date().getTime();
         if(now - lastCall >= delay){
+            slowSendTime.textContent = "";
             lastCall = now;
             return func(...args);
         }
         alert("Slow Mode you are with in the time")
+        slowSendTime.textContent = `You can send next message after ${messageInterval.value} seconds`
     }
 }
 
@@ -46,3 +49,54 @@ function sendChatMessage(){
     }
 
 }
+
+
+
+
+// Debounce Functionality
+
+const messageInputDebounce = document.getElementById("message-debounce");
+const messageFormDebounce = document.getElementById("messageForm-debounce");
+const messageContainerDebounce = document.querySelector(".messages-debounce");
+
+function debounce(func, delay){
+        let timeoutId;
+        return function(...args){
+            clearInterval(timeoutId);
+            timeoutId = setTimeout(()=>{
+                func(...args);
+            }, delay);
+        }
+}
+
+messageInputDebounce.addEventListener("input", debounce(handleInput, 500));
+
+function handleInput(event){
+    console.log("User is typing: ", event.target.value);
+}
+
+messageFormDebounce.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendChatMessageDebounce();
+});
+
+function sendChatMessageDebounce(){
+
+    const messageText = messageInputDebounce.value.trim();
+
+    if(messageText){
+        const messageElement = document.createElement("div");
+        messageElement.textContent = messageText;
+        messageElement.style.backgroundColor="black";
+        messageElement.style.color="white";
+        messageElement.style.padding="4px";
+        messageElement.style.borderRadius="5px"
+        messageElement.style.width="fit-content";
+        messageElement.style.margin="5px";
+        messageContainerDebounce.appendChild(messageElement);
+        messageInputDebounce.value = "";
+        messageInputDebounce.focus();
+        messageContainerDebounce.scrollTop = messageContainerDebounce.scrollHeight;
+    }
+
+}   
